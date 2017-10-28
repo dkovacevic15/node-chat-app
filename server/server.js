a41db5caea19ccd817bec87a9bf6c24840051d69
Dusan Kovacevic <dkovacevic15@raf.rs>
2017-10-28 22:01:17 +0200

    Add basic socket.io support

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
 var app = express();
-
 app.use(express.static(publicPath));
 
-app.listen(port, () => console.log(`server is up on port ${port}`));
+var server = http.createServer(app);
+
+var io = socketIO(server);
+io.on('connection', (socket) => {
+  console.log('New user connected!');
+});
+
+
+server.listen(port, () => console.log(`server is up on port ${port}`));
